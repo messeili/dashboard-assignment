e77bd4f0b51b66c2b473fd32cd381d95ff76b85c
messeili <mymoa.2014@gmail.com>
2021-01-09 03:55:51 +0200

    add charts

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,130 +1,135 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div>
+    <div class="chart-wrapper">
+      <apexchart width="800" type="pie" :options="options3" :series="series2">
+      </apexchart>
+    </div>
+    <div class="chart-wrapper">
+      <apexchart width="800" type="bar" :options="options" :series="series">
+      </apexchart>
+    </div>
+    <hr />
+    <div class="chart-wrapper">
+      <apexchart width="800" type="area" :options="options" :series="series">
+      </apexchart>
+    </div>
+    <hr />
+    <div class="chart-wrapper">
+      <apexchart width="800" type="bar" :options="options2" :series="series">
+      </apexchart>
+    </div>
+    <hr />
   </div>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String
-  }
+  data: () => ({
+    options: {
+      title: {
+        text: "Chart",
+        align: "center",
+        style: {
+          fontSize: "30px"
+        }
+      },
+      chart: {
+        id: "vuechart-example"
+      },
+      colors: ["#2E93fA"],
+
+      xaxis: {
+        categories: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec"
+        ]
+      }
+    },
+    options2: {
+      title: {
+        text: "Chart",
+        align: "center",
+        style: {
+          fontSize: "30px"
+        }
+      },
+      chart: {
+        id: "vuechart-example"
+      },
+      plotOptions: {
+        bar: {
+          barHeight: "100%",
+          distributed: true,
+          horizontal: true,
+          dataLabels: {
+            position: "bottom"
+          }
+        }
+      },
+      colors: [
+        "#33b2df",
+        "#546E7A",
+        "#d4526e",
+        "#13d8aa",
+        "#A5978B",
+        "#2b908f",
+        "#f9a3a4",
+        "#90ee7e",
+        "#f48024",
+        "#69d2e7"
+      ],
+      xaxis: {
+        categories: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec"
+        ]
+      }
+    },
+    options3: {
+      chartOptions: {
+        chart: {
+          width: 380,
+          type: "pie",
+          labels: ["Team A", "Team B", "Team C", "Team D", "Team E"]
+        }
+      }
+    },
+    series: [
+      {
+        data: [55, 62, 89, 66, 98, 72, 101, 75, 94, 120, 117, 139]
+      }
+    ],
+    series2: [44, 55, 13, 43, 22]
+  })
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+div.chart-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,34 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
+const state = {
+  posts: [],
+};
+
+//to handle state
+const getters = {};
+
+//to handle actions
+const actions = {
+  getPosts({ commit }) {
+    axios.get('https://run.mocky.io/v3/aa3c6047-0375-4774-93b6-87408af19e13').then((response) => {
+      commit('setData', response);
+    });
+  },
+};
+
+//to handle mutations
+const mutations = {
+  setData(state, posts) {
+    state.posts = posts;
+  },
+};
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state,
+  mutations,
+  actions,
+  getters,
 });
